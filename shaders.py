150c0e0f946a4b6f42c627a2eca1e2ff2f10a77d
veggiebob <veggiebobowman@gmail.com>
2020-03-27 14:31:34 -0600

    other minor changes

--- shaders.py
+++ shaders.py
@@ -8,44 +8,9 @@ from uniforms import *
 from array import array
 import numpy as np
 
-vertex_shader_string = """
-#version 330
-layout(location = 0) in vec3 position;
-layout(location = 1) in vec3 normal;
-layout(location = 2) in vec3 color;
-varying vec3 fposition;
-varying vec3 fnormal;
-out vec3 fcolor;
-uniform mat4 modelViewMatrix;
-uniform mat4 projectionMatrix;
-uniform mat4 viewMatrix;
-void main()
-{
-    fposition = position;
-    fnormal = normal;
-    vec4 cameraPos = vec4(position, 1.0);
-    gl_Position = projectionMatrix * viewMatrix * modelViewMatrix * vec4(position, 1.);
-    fcolor = color;
-}
-"""
+vertex_shader_string = open('data/shaders/vertex.glsl', 'r').read()
 
-fragment_shader_string = """
-#version 330
-out vec4 outputColor;
-varying vec3 fposition;
-varying vec3 fnormal;
-varying vec3 fcolor;
-uniform sampler2D noise_512;
-uniform sampler2D cat;
-void main()
-{
-    vec3 col = texture2D(noise_512, fposition.xy).r * fcolor;
-    if (dot(vec3(1., 0., 0.), fnormal) > 0.5) {
-        col = texture2D(cat, fposition.xy + 0.5).rgb;
-    }
-    outputColor = vec4(col, 1.);
-}
-"""
+fragment_shader_string = open('data/shaders/fragment.glsl', 'r').read()
 
 def create_all_shaders():
     program = glCreateProgram()
