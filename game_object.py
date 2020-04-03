fixed rotation stuff

game_object.py
@@ -99,9 +99,11 @@ class RenderableObject:
 
     def get_rotation_matrix (self):
         self.heading = vertex_math.euler(self.euler_rot[0], self.euler_rot[1], self.euler_rot[2], np.array([1, 0, 0], dtype='float32'))
+        norm = vertex_math.euler(self.euler_rot[0], self.euler_rot[1], self.euler_rot[2], np.array([0, 1, 0], dtype='float32'))
+        # print(self.heading.dot(RenderableObject.UP))
         # print(self.heading)
         T = vertex_math.norm_vec3(self.heading)
-        N = -np.cross(RenderableObject.UP, self.heading)
+        N = norm # -np.cross(RenderableObject.UP, self.heading)
         if N[0] == 0 and N[1] == 0 and N[2] == 0:
             N = np.array([1, 0, 0], dtype='float32') # DON'T LOOK UP!!
         B = np.cross(N, T)

main.py
@@ -81,6 +81,7 @@ def render():
     # test_obj.translation[1] = np.cos(framecount * 0.01)
     # test_obj.translation[2] = np.sin(framecount * 0.01)
     test_obj.euler_rot[1] = framecount * 0.002
+    # test_obj.euler_rot[2] = -np.pi / 2
     # test_obj_2.render()
     test_obj.render()
 
@@ -130,7 +131,7 @@ def main():
         }
     })
 
-    test_obj = obj_loader.load_game_object_from_file('data/models/cowboy.obj', program, scale=2, color=[0.0, 0.0, 0.0])
+    test_obj = obj_loader.load_game_object_from_file('data/models/sphere.obj', program, scale=2, color=[1.0, 0.0, 0.0])
     # test_obj_2 = obj_loader.load_game_object_from_file('data/models/teapot.obj', program, scale=1/50, color=[0, 0, 1])
 
     glutDisplayFunc(render)